68d59618e7a9814faff02d1f2612df397e53708a
Nyx <mail@lemonandlime.work.gd>
2026-06-16 13:12:08 +0000

    Config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://fatetubeproxy.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDk5MmNlMjQxMmQ0NGY1MjM3ODBhZWJhYmUwOWVkMSIsIm5iZiI6MTc0OTMxODUwOS44NzIsInN1YiI6IjY4NDQ3YjZkOTVhYzA2YTczZTIwNWFjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j7QJ7RhVrED59HZKfiurVFND3-TQT6MLuhP48HtDas0",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: true,
 
   // The backend URL(s) to communicate with - can be a single URL or comma-separated list (e.g., "https://server1.com,https://server2.com")
-  VITE_BACKEND_URL: null,
+  VITE_BACKEND_URL: "https://fatetubebackend1.netlify.app",
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
